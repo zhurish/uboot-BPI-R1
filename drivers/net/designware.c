81695e0438ad557036c3feda5d88502c93f5f82c
zhurish <zhurish@163.com>
2017-12-19 23:28:36 +0800

    BPI-R1 UBOOT Support network

--- drivers/net/designware.c
+++ drivers/net/designware.c
@@ -342,7 +342,7 @@ int designware_eth_enable(struct dw_eth_dev *priv)
 
 	return 0;
 }
-
+#define ETH_ZLEN	60 
 static int _dw_eth_send(struct dw_eth_dev *priv, void *packet, int length)
 {
 	struct eth_dma_regs *dma_p = priv->dma_regs_p;
@@ -368,7 +368,7 @@ static int _dw_eth_send(struct dw_eth_dev *priv, void *packet, int length)
 		printf("CPU not owner of tx frame\n");
 		return -EPERM;
 	}
-
+	length = (length < ETH_ZLEN) ? ETH_ZLEN : length;
 	memcpy((void *)data_start, packet, length);
 
 	/* Flush data to be sent */
--- include/phy.h
+++ include/phy.h
@@ -256,7 +256,9 @@ int gen10g_config(struct phy_device *phydev);
 int gen10g_startup(struct phy_device *phydev);
 int gen10g_shutdown(struct phy_device *phydev);
 int gen10g_discover_mmds(struct phy_device *phydev);
-
+#ifdef CONFIG_B53_SWITCH 
+int phy_b53_init(void);
+#endif
 int phy_mv88e61xx_init(void);
 int phy_aquantia_init(void);
 int phy_atheros_init(void);
